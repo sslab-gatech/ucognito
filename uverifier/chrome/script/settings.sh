485683fb4582bf10db9c140a8d72b2963e80b870
Meng Xu <meng.xu@gatech.edu>
2015-11-01 19:30:53 -0500

    separate out ucognito and uverifier code and scripts

--- uverifier/chrome/script/settings.sh
+++ uverifier/chrome/script/settings.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-WORKSPACE=$(readlink -m "../../")
+WORKSPACE=$(readlink -m "../../../")
 
 URL_LIST=$WORKSPACE/url/urllist
 
-FIREFOX_BIN=$WORKSPACE/firefox/build/firefox
+# XXX please change
+CHROME_BIN=
 
-TRACE_LOG=$WORKSPACE/firefox/strace/syscall.log
+TRACE_LOG=$WORKSPACE/uverifier/chrome/strace/syscall.log
 
-PROFILE_DIR=$WORKSPACE/firefox/profile
+PROFILE_DIR=$WORKSPACE/uverifier/chrome/profile
 PROFILE_EMPTY=$PROFILE_DIR/empty
 PROFILE_PBTEST=$PROFILE_DIR/pbtest
 
 STRACE_SET="open,close,read,write,mmap,stat,fstat,lstat,preadv,pwritev,pread64,pwrite64,readv,writev,access,dup,dup2,dup3,clone,fork,vfork,execve,truncate,ftruncate,mkdir,creat,link,unlink,symlink,readlink,rename,openat,mkdirat,mknodat,unlinkat,renameat,linkat,symlinkat,readlinkat,faccessat"
 
-FIREFOX_LAUNCH_TIME=3
+CHROME_LAUNCH_TIME=3
 WEBPAGE_LOAD_TIME=20
 LOG_BUFFERING_TIME=3
 
--- uverifier/firefox/script/settings.sh
+++ uverifier/firefox/script/settings.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-WORKSPACE=$(readlink -m "../../")
+WORKSPACE=$(readlink -m "../../../")
 
 URL_LIST=$WORKSPACE/url/urllist
 
-CHROME_BIN=/opt/google/chrome/chrome
+# XXX please change
+FIREFOX_BIN=
 
-TRACE_LOG=$WORKSPACE/chrome/strace/syscall.log
+TRACE_LOG=$WORKSPACE/uverifier/firefox/strace/syscall.log
 
-PROFILE_DIR=$WORKSPACE/chrome/profile
+PROFILE_DIR=$WORKSPACE/uverifier/firefox/profile
 PROFILE_EMPTY=$PROFILE_DIR/empty
 PROFILE_PBTEST=$PROFILE_DIR/pbtest
 
 STRACE_SET="open,close,read,write,mmap,stat,fstat,lstat,preadv,pwritev,pread64,pwrite64,readv,writev,access,dup,dup2,dup3,clone,fork,vfork,execve,truncate,ftruncate,mkdir,creat,link,unlink,symlink,readlink,rename,openat,mkdirat,mknodat,unlinkat,renameat,linkat,symlinkat,readlinkat,faccessat"
 
-CHROME_LAUNCH_TIME=3
+FIREFOX_LAUNCH_TIME=3
 WEBPAGE_LOAD_TIME=20
 LOG_BUFFERING_TIME=3
 
